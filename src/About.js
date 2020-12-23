42bd97aca48e6624a195bd01df447b76b78aab11
aamirali <sufaamir@gmail.com>
2020-12-23 14:53:33 -0500

    update files

--- src/About.js
+++ src/About.js
@@ -1,18 +1,29 @@
 import React from 'react'
-import History from './history'
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import AamirPircure from './img/Aamirpicture.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(10),
+    },
+  },
+  large: {
+    width: theme.spacing(37),
+    height: theme.spacing(37),
+    marginLeft: 570,
+    },
+}));
+
 
 function About() {
-  const myFunc = () => {
-    console.log("myFunc")
-    History.push('/Product')
-  }
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       About PAGE
-      <button onClick={myFunc}>
-        function
-      </button>
-
+      <Avatar alt="Aamir Ali Picture" src={AamirPircure} className={classes.large}/>
     </div>
   )
 }
