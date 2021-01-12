ec1b323fcd5e800fe143f4c72a99f2b305ba1dd1
aamirali <sufaamir@gmail.com>
2021-01-12 14:20:05 -0500

    update menubar

--- src/Head.js
+++ src/Head.js
@@ -1,35 +1,30 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
 import HomeIcon from "@material-ui/icons/HomeTwoTone";
-import { Container } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import InfoIcon from '@material-ui/icons/Info';
-import './App.css'
 import ContactusButton from "./contactusbutton";
+import { Menu } from '@material-ui/icons'
 
 
 function Header() {
   return (
-      <Container maxWidth='xl' className="headerInline" style={{backgroundColor: 'skyblue'}}>
-      <ul>
-        <li>
-          <h3> Shoe Store</h3>
-        </li>
-        <li>
-          <NavLink exact activeClassName="active" to="/"><HomeIcon color="primary"/>Home</NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName="active" to="/about"><InfoIcon color="primary"/>About</NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName="active" to="/Product"><AddShoppingCartIcon color="secondary"/>Products</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contactus"><ContactusButton/></NavLink>
-        </li>
-      </ul>
-      
-      </Container>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="inherit" edge="start" aria-label="menu">
+            <Menu />
+          </IconButton>
+          <Typography variant='h4' style={{ flexGrow: 1 }}>Shoe Store</Typography>
+          <Button color="inherit"><NavLink exact activeClassName="active" to="/"><HomeIcon />Home</NavLink></Button>
+          <Button color="inherit"><NavLink activeClassName="active" to="/about"><InfoIcon />About</NavLink></Button>
+          <Button color="inherit"><NavLink activeClassName="active" to="/Product"><AddShoppingCartIcon />Products</NavLink></Button>
+          <NavLink to="/contactus"><ContactusButton /></NavLink>
+
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 export default Header;
